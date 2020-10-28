58f3bfb36e4ef5f00123e9bf07f2f65da46d16e4
Ricardo Ortiz <ricardo.mpcs@gmail.com>
2020-10-28 00:21:44 -0500

    Guard service implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,15 @@ import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import { ErrorComponent } from './error/error.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
+import { RouteGuardService } from './service/route-guard.service';
 
 
 const routes: Routes = [
   {path: '', component:LoginComponent},
-  {path: 'welcome/:user', component:WelcomeComponent},
+  {path: 'welcome/:user', component:WelcomeComponent, canActivate:[RouteGuardService]},
   {path:'login',component:LoginComponent},
-  {path: 'logout', component:LogoutComponent},
-  {path:'todos',component:TodoListComponent},
+  {path: 'logout', component:LogoutComponent, canActivate:[RouteGuardService]},
+  {path:'todos',component:TodoListComponent, canActivate:[RouteGuardService]},
   {path:'**', component:ErrorComponent}
 ];
 
